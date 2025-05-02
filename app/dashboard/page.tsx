baf6abf207d599c618e0eaa40c610585ef43818c
pranto mollick <prantocse01@gmail.com>
2025-05-02 18:50:17 +0600

    we are finish our login route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,7 @@
-import { redirect } from "next/navigation";
-import { auth } from "../utils/auth";
+import { requireUser } from "../utils/hooks";
 
 export default async function DashboardPage() {
-    const session = await auth();
-
-    if (!session || !session?.user) {
-        redirect("/login");
-    }
-
+    const session = await requireUser();
     return (
         <div className="h-screen w-screen flex items-center justify-center px-4">
             <h1 className="text-2xl">Dashboard</h1>
